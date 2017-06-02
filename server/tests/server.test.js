af4bbfb35b52361c0747974664f78df5b405e71c
nishkaush <Nishant@Nishants-MacBook-Air.local>
2017-06-02 21:21:08 +1000

    add tests for delete /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -340,3 +340,26 @@ describe("POST /users/login", () => {
     });
 
 });
+
+// -----------------------------------------------------------------
+
+
+describe("DELETE /users/me/token", () => {
+
+    it("should remove auth token on logout", (done) => {
+        request(app)
+            .delete("/users/me/token")
+            .set("x-auth", usersArr[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                users.findById(usersArr[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+});
